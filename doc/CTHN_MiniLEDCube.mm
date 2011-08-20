9e70dfbab3d1b1d67143ababff286c3f272d5bdc
Kai Lauterbach <klaute@web.de>
2011-08-20 15:20:26 +0200

    Defined some files to ignore. Added some documentation

---
+++ doc/CTHN_MiniLEDCube.mm
@@ -0,0 +1,11 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1313834374248" ID="ID_482134697" MODIFIED="1313834387274" TEXT="CTHN.de MiniLEDCube">
+<node CREATED="1313834388919" ID="ID_340649650" MODIFIED="1313834393990" POSITION="right" TEXT="Offene Fragen">
+<node CREATED="1313834404986" ID="ID_1744832341" MODIFIED="1313834423419" TEXT="Ist genug Platz auf dem Tiny f&#xfc;r V-USB?"/>
+<node CREATED="1313834445484" ID="ID_678170530" MODIFIED="1313834450899" TEXT="Wie Leiterbahnen legen?"/>
+</node>
+<node CREATED="1313834395025" ID="ID_22675793" MODIFIED="1313834396759" POSITION="left" TEXT="Bauteile"/>
+<node CREATED="1313834398403" ID="ID_215502658" MODIFIED="1313834401587" POSITION="right" TEXT="Aufbau"/>
+</node>
+</map>
