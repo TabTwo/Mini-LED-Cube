430706747c54467ec2d602de8239c62d929205de
Kai Lauterbach <klaute@web.de>
2011-12-28 01:19:35 +0100

    Constants added.

---
+++ client/globals.h
@@ -0,0 +1,16 @@
+/*
+ * CTHN.de MiniLEDCube
+ *
+ *  By Kai Lauterbach (klaute at web dot de) 12/2011
+ *
+ *  Based on http://mosfetkiller.de/?s=miniledcube
+ *
+ *  License: General Public License (GPL v3)
+ *
+ */
+
+#define NOT_CONNECTED_ERROR    -1
+#define DEVICE_NOT_FOUND_ERROR -2
+
+#define SUCCESSFULLY_CONNECTED  1
+
